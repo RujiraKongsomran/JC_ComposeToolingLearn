1d8d1f01f14779bd04fa1c8d42c6717ad4a1a1e6
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-04-11 16:41:07 +0700

    MultiPreview Annotation ThemesPreview

--- app/src/main/java/com/rujirakongsomran/jc_composetoolinglearn/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_composetoolinglearn/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.rujirakongsomran.jc_composetoolinglearn
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,13 +37,11 @@ class MainActivity : ComponentActivity() {
     name = "small font",
     group = "font scales",
     fontScale = 0.5f,
-    showBackground = true
 )
 @Preview(
     name = "large font",
     group = "font scales",
     fontScale = 1.5f,
-    showBackground = true
 )
 annotation class FontScalePreviews
 
@@ -62,6 +62,23 @@ fun LocaleParameter() {
         Text(text = stringResource(id = R.string.name))
     }
 }
+
+@Preview(showBackground = true)
+@Preview(
+    name = "dark theme",
+    group = "themes",
+    uiMode = UI_MODE_NIGHT_YES
+)
+annotation class ThemesPreview
+
+@ThemesPreview
+@Composable
+fun ThemesPreviewDisplay() {
+    OutlinedButton(onClick = { /*TODO*/ }) {
+        Text(text = "MaMa")
+    }
+}
+
 
 
 
